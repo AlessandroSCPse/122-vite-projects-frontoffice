e3b9b601f93dc7d34321cd7102fc631fd4f8371c
AlessandroSCPse <scolozzi.alessandro@gmail.com>
2024-06-12 11:40:52 +0200

    projects pagination

--- src/pages/ProjectsList.vue
+++ src/pages/ProjectsList.vue
@@ -6,22 +6,32 @@ export default {
     name: 'ProjectsList',
     data() {
         return {
-            projects: []
+            projects: [],
+            currentPage: 1,
+            nextPageUrl: null,
+            prevPageUrl: null
         };
     },
     components: {
         ProjectCard
     },
     methods: {
-        getProjectsFromApi() {
-            axios.get('http://127.0.0.1:8000/api/projects')
+        getProjectsFromApi(dataPage) {
+            axios.get('http://127.0.0.1:8000/api/projects', {
+                params: {
+                    page: dataPage
+                }
+            })
             .then((response) => {
-                this.projects = response.data.results;
+                this.projects = response.data.results.data;
+                this.currentPage = response.data.results.current_page;
+                this.nextPageUrl = response.data.results.next_page_url;
+                this.prevPageUrl = response.data.results.prev_page_url;
             });
         }
     },
     mounted() {
-        this.getProjectsFromApi();
+        this.getProjectsFromApi(this.currentPage);
     }
 }
 </script>
@@ -35,5 +45,17 @@ export default {
                 <ProjectCard :projectInfo="project"></ProjectCard>
             </div>
         </div>
+        
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li v-if="prevPageUrl" class="page-item">
+                    <a class="page-link" @click="getProjectsFromApi(currentPage - 1)">Previous</a>
+                </li>
+
+                <li v-if="nextPageUrl" class="page-item">
+                    <a class="page-link" @click="getProjectsFromApi(currentPage + 1)">Next</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
