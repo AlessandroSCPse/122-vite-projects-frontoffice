7de75b21fa09e1105b10eac730851091a7fbdd0e
AlessandroSCPse <scolozzi.alessandro@gmail.com>
2024-06-12 10:15:07 +0200

    Single Project page base scaffolding

---
+++ src/pages/SingleProject.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'SingleProject'
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>Project name</h1>
+
+        <div>test: {{ $route.params.slug }}</div>
+    </div>
+</template>
