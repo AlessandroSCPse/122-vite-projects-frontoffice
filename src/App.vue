a8f39c4544ebfc335779d439bbcdb6ca5ee97dd1
AlessandroSCPse <scolozzi.alessandro@gmail.com>
2024-06-12 10:08:27 +0200

    done projects page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script>
-import ProjectsList from './components/ProjectsList.vue';
 import AppHeader from './components/AppHeader.vue';
 
 export default {
   components: {
-    ProjectsList,
     AppHeader
   }
 }
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -15,7 +15,15 @@ export default {
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">Home</a>
+                            <router-link class="nav-link" aria-current="page" :to="{name: 'home'}">Home</router-link>
+                        </li>
+
+                        <li class="nav-item">
+                            <router-link class="nav-link" aria-current="page" :to="{name: 'about'}">About</router-link>
+                        </li>
+
+                        <li class="nav-item">
+                            <router-link class="nav-link" aria-current="page" :to="{name: 'projects'}">Projects</router-link>
                         </li>
                     </ul>
                 </div>
